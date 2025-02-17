fe8469d3b5c936e6dbd0d43095cd00b42183952d
Gabriel Moreira <12300870@aluno.cotemig.com.br>
2025-02-17 19:33:38 -0300

    Delete ativadade de algoritmos-sintaxe-kotlin directory

--- ativadade de algoritmos-sintaxe-kotlin/ListaESE02.kt
+++
@@ -1,6 +0,0 @@
-@file:JvmName("JDoodle")
-fun main() {
-     print("Por favor, digite um número: ")
-    val num = readln()!!.toInt()
-    println("O número informado foi $num")
-}
--- ativadade de algoritmos-sintaxe-kotlin/ListaESE03.kt
+++
@@ -1,9 +0,0 @@
-@file:JvmName("JDoodle")
-fun main() {
-     print("Por favor, digite o primeiro número: ")
-    val num1 = readln()!!.toInt()
-    print("Por favor, digite o segundo número: ")
-    val num2 = readln()!!.toInt()
-    val soma = num1+num2
-    println("A soma deu: $soma")
-}
